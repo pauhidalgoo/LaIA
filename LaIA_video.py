595ec753c1b5e2f504091dd4cee4d064b28f5e04
rogerbaiges <rogerbaigestrilla@gmail.com>
2024-11-09 11:56:51 +0100

    MOD Cai name to cAI

--- LaIA_video.py
+++ LaIA_video.py
@@ -174,6 +174,7 @@ class LaIA_video:
         def detect_selected_category(text, categories):
             for category in categories:
                 if category.lower() in text.lower():
+                    print(f"Selected category: {category}")
                     return category
             return random.choice(categories)
 
@@ -225,6 +226,8 @@ class LaIA_video:
         max_text_width = 60
 
         for start, end, speaker, text in self.time_stamps:
+            if speaker == 'Cai':
+                speaker = 'cAI'
             # Add speaker name and wrap text with quotes
             formatted_text = f'{speaker}: "{text.strip()}"'
             wrapped_text = "\n".join(wrap(formatted_text, width=max_text_width))
@@ -250,55 +253,3 @@ class LaIA_video:
                 os.unlink(file_path)
 
         final_video.write_videofile(output_video, codec="libx264", audio_codec="aac", temp_audiofile="temp-audio.m4a", remove_temp=True, verbose=False)
-
-
-
-dialogue_text = """
-Cai: Bon dia, LaIA. M'agradaria saber com funciona el procés de sol·licitud de la beca Equitat.
-
-LaIA: Bon dia, Cai. La beca Equitat és una ajuda econòmica que s'ofereix a estudiants de grau i màster habilitant de les universitats públiques de Catalunya, la UOC i alguns centres adscrits.
-
-Cai: Com funciona el procés de sol·licitud?
-
-LaIA: El procés de sol·licitud es realitza exclusivament per internet i el termini per al curs 2024-2025 és del 16 de setembre al 31 d'octubre de 2024 a les 14:00 h (hora local de Barcelona).
-
-Cai: Quins requisits he de complir per a sol·licitar la beca?
-
-LaIA: Per a sol·licitar la beca Equitat, cal que siguis un estudiant de grau o màster habilitant de les universitats públiques de Catalunya, la UOC o alguns centres adscrits que hi participen.
-
-Cai: Com sé si puc optar a la beca?
-
-LaIA: Per a saber si pots optar a la beca Equitat, has de demanar la beca i, posteriorment, l'AGAUR t'informarà de quin tram de renda familiar et correspon.
-
-Cai: Com es demana la beca?
-
-LaIA: La sol·licitud es realitza per via electrònica i has d'accedir a través de l'apartat "Tràmits gencat" del web de la Generalitat de Catalunya o des de la pàgina web de l'AGAUR.
-
-Cai: Com sé l'estat de la meva beca?
-
-LaIA: La tramitació de les sol·licituds de beques gestionades per l'AGAUR es realitza a través del portal Tràmits gencat, amb el codi identificador (codi ID) associat a la sol·licitud.
-
-Cai: Què és el codi ID?
-
-LaIA: El codi ID és un codi identificador associat a la sol·licitud de la beca i consta en el resguard de la beca sol·licitada.
-
-Cai: Com sé si la beca cobreix el preu de la matrícula?
-
-LaIA: La beca Equitat cobreix la minoració del preu de la matrícula, però no cobreix els crèdits matriculats per segona i successives vegades, els crèdits convalidats, reconeguts i/o adaptats i les quotes de l'assegurança escolar o qualsevol altra associada a la matrícula.
-
-Cai: És compatible amb la beca general del Ministeri?
-
-LaIA: La beca Equitat és incompatible amb la beca general del Ministeri.
-
-Cai: És recomanable demanar les dues beques?
-
-LaIA: Sí, és recomanable demanar les dues beques, ja que la beca Equitat complementa la beca general del Ministeri.
-
-Cai: Moltes gràcies, LaIA.
-
-LaIA: De res, Cai. Si tens més preguntes, no dubtis a consultar-me.
-"""
-
-# Example
-
-LaIA_video(dialogue_text=dialogue_text, dialect='central')
